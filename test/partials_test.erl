db5cbf5ad0a52f209e40ef71f26e06141c2bc89c
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-06 11:12:05 +0300

    Add failing test for partials

---
+++ test/partials_test.erl
@@ -0,0 +1,45 @@
+-module(partials_test).
+
+-compile([export_all]).
+
+basic_test() ->
+    HelloTemplate = "Hello {{{name}}}.\n"
+                    "Drinks:",
+    Template = "{{> hello }}\n"
+               "{{#drinks}}\n"
+               "    - {{name}}, {{tastiness}}\n"
+               "{{/drinks}}",
+    Context = [{name, "Devin & Jane"},
+               {drinks, [[{name, "Beer"},
+                          {tastiness, 5}],
+                         [{name, "Juice"},
+                          {tastiness, 8}]]}],
+    Expected = <<"Hello Devin & Jane.\n"
+                 "Drinks:\n\n"
+                 "    - Beer, 5\n\n"
+                 "    - Juice, 8\n">>,
+    Expected = walrus:render(Template, Context, [{hello, HelloTemplate}]).
+
+nested_test() ->
+    MainTemplate = "My name is {{name}}\n"
+                   "I plan to travel into following places:\n\n"
+                   "{{#places}}\n"
+                   "  {{> place_item }}\n"
+                   "{{/places}}",
+    
+    PlaceItemTemplate = "{{name}} ({{country}}):\n{{> place_description }}",
+    PlaceDescTemplate = "  {{description}}",
+    Context = [{name, "Vladimir"},
+               {places, [
+                 [{name, "San Jose"}, {country, "USA"}, {description, "Very interesting place"}],
+                 [{name, "Kiev"}, {country, "Ukraine"}, {description, "Great place"}]
+    ]}],
+    PartialsContext = [{place_item, PlaceItemTemplate}, {place_description, PlaceDescTemplate}],
+    Expected = "My name is Vladimir\n"
+               "I plan to travel into following places:\n\n"
+               "San Jose (USA):"
+               "  Very interesting place"
+               "Kiev (Ukraine):"
+               "  Great place place",
+    Expected = walrus:render(MainTemplate, Context, PartialsContext).
+    
